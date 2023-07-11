80f8470089c864ac454a9ebbdde58818ef5dcf92
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-07-11 22:16:12 +0530

    Update 2D_array_dynamic.c

--- 07_array_tasks/2D_array/2D_array_dynamic.c
+++ 07_array_tasks/2D_array/2D_array_dynamic.c
@@ -22,8 +22,36 @@ int main(void)
 
     for (iCounter1 = 0; iCounter1 < iRows; iCounter1++)
     {
-        ppPtr[iCounter]
+        ppPtr[iCounter1] = (int *)malloc(iColumns * sizeof(int));
+        if (ppPtr[iCounter1] == NULL)
+        {
+            puts("Memory allocation FAILED");
+            exit(-1);
+        }
+
+        for (iCounter2 = 0; iCounter2 < iColumns; iCounter2++)
+        {
+            printf("Enter [%d][%d] value:\t", iCounter1, iCounter2);
+            scanf("%d", &ppPtr[iCounter1][iCounter2]);
+        }
     }
+
+    printf("Entered elements are:\n");
+
+    for (iCounter1 = 0; iCounter1 < iRows; iCounter1++)
+    {
+        for (iCounter2 = 0; iCounter2 < iColumns; iCounter2++)
+            printf("[%d][%d] value is:\t%d\n", iCounter1, iCounter2, ppPtr[iCounter1][iCounter2]);
+    }
+
+    for (iCounter1 = 0; iCounter1 < iRows; iCounter1++)
+    {
+        free(ppPtr[iCounter1]);
+        ppPtr[iCounter1] = NULL;
+    }
+
+    free(ppPtr);
+    ppPtr = NULL;
 
     exit(0);
 }
