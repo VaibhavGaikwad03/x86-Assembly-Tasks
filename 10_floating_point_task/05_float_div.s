d143f9fbb244c0a7066653e9a32f4b8b2de5692c
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-08-22 22:49:09 +0530

    Add files via upload

---
+++ 10_floating_point_task/05_float_div.s
@@ -0,0 +1,91 @@
+.section .rodata
+    msg_main_print_fNo1_fNo2:
+    .string "fNo1 = %f\nfNo2 = %f\n"
+
+    msg_main_print_fAns1:
+    .string "fAns1 = %f\n"
+
+    msg_main_print_fAns2:
+    .string "fAns2 = %f\n"
+
+    msg_main_print_fAns3:
+    .string "fAns3 = %f\n"
+
+    int_2:
+    .int    2
+
+.section .data
+.globl  fNo1
+.type   fNo1, @object
+.size   fNo1, 4
+.align  4
+fNo1:
+    .float  10.22
+
+.globl  fNo2
+.type   fNo2, @object
+.size   fNo2, 4
+.align  4
+fNo2:
+    .float  20.22
+
+.section .bss
+    .comm   fAns1, 4, 4
+    .comm   fAns2, 4, 4
+    .comm   fAns3, 4, 4
+
+.section .text
+.globl  main
+.type   main, @function
+main:
+    andl    $-16, %esp
+    pushl   %ebp
+    movl    %esp, %ebp
+
+    finit
+
+    flds    fNo2
+    flds    fNo1
+
+    movl    $msg_main_print_fNo1_fNo2, (%esp)
+    fstpl   4(%esp)
+    fstpl   12(%esp)
+    call    printf
+
+    flds    fNo1
+    flds    fNo2
+
+    fdivp    %st(0), %st(1)
+
+    fstps   fAns1
+
+    flds    fNo2
+    flds    fNo1
+
+    fdiv    %st(1), %st(0)
+
+    fstps   fAns2
+
+    flds    fNo1
+
+    fidivs  int_2
+
+    fstps   fAns3
+
+    flds    fAns1
+    movl    $msg_main_print_fAns1, (%esp)
+    fstpl   4(%esp)
+    call    printf
+
+    flds    fAns2
+    movl    $msg_main_print_fAns2, (%esp)
+    fstpl   4(%esp)
+    call    printf
+
+    flds    fAns3
+    movl    $msg_main_print_fAns3, (%esp)
+    fstpl   4(%esp)
+    call    printf
+
+    movl    $0, (%esp)
+    call    exit
