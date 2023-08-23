a4d65f4f1c9b3ca0aa9656423a501f94e5c1ce5c
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-08-23 23:13:07 +0530

    Add files via upload

---
+++ 10_floating_point_task/06_all_mix_float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float fAns;
+
+    fAns = ((43.65 / 22) + (76.34 * 3.1)) / ((12.43 * 6) - (140.2 / 94.21));
+
+    printf("Calculation is : %f\n", fAns);
+
+    exit(0);
+}
