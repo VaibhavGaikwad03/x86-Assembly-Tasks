bb63c8be233e07d4ddea2d0c5f3f8b10850726f7
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-09-06 08:26:39 +0530

    Add files via upload

---
+++ 13_file_handling_using_c_library_functions/04_file_copy_with_cla.s
@@ -0,0 +1,100 @@
+.section .rodata
+    msg_main_read_mode:
+    .string "r"
+
+    msg_main_print_cant_open_source_file:
+    .string "\nCan't Open Source File\n"
+
+    msg_main_write_mode:
+    .string "w"
+
+    msg_main_print_cant_open_destination_file:
+    .string "\nCan't Open Destination File\n"
+
+    msg_main_print_copied_successfully:
+    .string "\nCopied Successfully\n"
+
+.section .text
+.globl  main 
+.type   main, @function
+main:
+    andl    $-16, %esp
+    pushl   %ebp
+    movl    %esp, %ebp
+
+    subl    $32, %esp
+    
+    movl    $0, -4(%ebp)
+    movl    $0, -8(%ebp)
+
+    movl    12(%ebp), %eax
+    movl    %eax, (%esp)
+    movl    $msg_main_read_mode, 4(%esp)
+    call    fopen
+    movl    %eax, -4(%ebp)
+
+    cmpl    $0, %eax
+    jne     label_if_1_out
+    movl    $msg_main_print_cant_open_source_file, (%esp)
+    call    printf 
+
+    movl    $-1, (%esp)
+    call    exit
+
+label_if_1_out:
+    movl    16(%ebp), %eax
+    movl    %eax, (%esp)
+    movl    $msg_main_write_mode, 4(%esp)
+    call    fopen 
+    movl    %eax, -8(%ebp)
+
+    cmpl    $0, %eax
+    jne     label_if_2_out
+    movl    $msg_main_print_cant_open_destination_file, (%esp)
+    call    printf
+
+    movl    -4(%ebp), %ebx
+    movl    %ebx, (%esp)
+    call    fclose 
+    movl    $0, -4(%ebp)
+
+    movl    $-1, (%esp)
+    call    exit 
+label_if_2_out:
+label_while_body:
+    movl    -4(%ebp), %ebx
+    movl    %ebx, (%esp)
+    call    fgetc
+    movb    %al, -9(%ebp)
+
+    cmpb    $-1, %al
+    jne     label_if_3_out
+    jmp     label_break_while_loop
+label_if_3_out:
+    movb    -9(%ebp), %al
+    movl    -8(%ebp), %edx
+
+    movb    %al, (%esp)
+    movl    %edx, 4(%esp)
+    call    fputc
+
+    jmp     label_while_body
+label_break_while_loop:
+
+    movl    -4(%ebp), %ebx
+    movl    %ebx, (%esp)
+    call    fclose
+    
+    movl    $0, -4(%ebp)
+
+    movl    -8(%ebp), %ebx
+    movl    %ebx, (%esp)
+    call    fclose
+
+    movl    $0, -8(%ebp)
+
+    movl    $msg_main_print_copied_successfully, (%esp)
+    call    printf
+
+    movl    $0, (%esp)
+    call    exit
