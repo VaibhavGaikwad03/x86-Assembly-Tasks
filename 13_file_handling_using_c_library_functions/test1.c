3398e5418b33b383f3bd53249da7d73efbee3242
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-09-09 18:30:21 +0530

    Add files via upload

---
+++ 13_file_handling_using_c_library_functions/test1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *fp = NULL;
+
+    struct student
+    {
+        char szName[40];
+        int iRollNo;
+        float fPerc;
+    };
+
+    struct student s;
+
+    fp = fopen("student_structure_from_keyboard_to_file.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("\nCan't open file\n");
+        exit(-1);
+    }
+
+    while (fscanf(fp,"%s %d %f", s.szName, &s.iRollNo, &s.fPerc) != EOF)  
+    // while (fread(&s, sizeof(s), 1, fp) == 1)
+        printf("%s %d %f\n", s.szName, s.iRollNo, s.fPerc);
+
+    fclose(fp);
+    fp = NULL;
+
+    exit(0);
+}
