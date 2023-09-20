52248c62540a36e62f75ef37c097ff15332e28f9
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-09-20 21:37:39 +0530

    Add files via upload

---
+++ 14_file_handling_using_system_calls/09_structure_from_file_to_screen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd;
+    int iReadBytes;
+
+    struct student
+    {
+        char szName[40];
+        int iRollNo;
+        float fPerc;
+    };
+
+    struct student s;
+
+    fd = open("student_structure_from_keyboard_to_file.txt", O_RDONLY);
+
+    if (fd == -1)
+    {
+        write(1, "\nCan't open file\n", 18);
+        exit(-1);
+    }
+
+    while(iReadBytes = read(fd, &s, sizeof(s)))
+        printf("%s %d %f\n", s.szName, s.iRollNo, s.fPerc);
+    
+    close(fd);
+
+    exit(0);
+}
